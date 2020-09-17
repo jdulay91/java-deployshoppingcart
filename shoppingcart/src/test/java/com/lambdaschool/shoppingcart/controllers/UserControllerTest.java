ae9f11664e679078ac4b42f744d685fa9b3d4122
Julliann Dulay <jullianndulay@gmail.com>
2020-09-17 15:26:46 -0700

    mvp met

--- shoppingcart/src/test/java/com/lambdaschool/shoppingcart/controllers/UserControllerTest.java
+++ shoppingcart/src/test/java/com/lambdaschool/shoppingcart/controllers/UserControllerTest.java
@@ -1,30 +1,145 @@
 package com.lambdaschool.shoppingcart.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.lambdaschool.shoppingcart.models.Role;
+import com.lambdaschool.shoppingcart.models.User;
+import com.lambdaschool.shoppingcart.models.UserRoles;
+import com.lambdaschool.shoppingcart.services.UserService;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
-
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@WithMockUser(username = "admin", roles = {"USER", "ADMIN"})
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class UserControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
 
-    @org.junit.Before
+    @MockBean
+    private UserService userService;
+
+    private List<User> userList = new ArrayList<>();
+
+    @Before
     public void setUp() throws Exception {
+        userList = new ArrayList<>();
+
+        Role r1 = new Role("admin");
+        r1.setRoleid(1);
+        Role r2 = new Role("user");
+        r2.setRoleid(2);
+        Role r3 = new Role("data");
+        r3.setRoleid(3);
+
+        // admin, data, user
+        User u1 = new User("admin", "ILuvM4th!", "admin@lambdaschool.local");
+        u1.getRoles().add(new UserRoles(u1, r1));
+        u1.getRoles().add(new UserRoles(u1, r2));
+        u1.getRoles().add(new UserRoles(u1, r3));
+
+        u1.setUserid(101);
+        userList.add(u1);
+
+        // data, user
+        ArrayList<UserRoles> datas = new ArrayList<>();
+        User u2 = new User("cinnamon", "1234567", "cinnamon@lambdaschool.local");
+        u1.getRoles().add(new UserRoles(u2, r2));
+        u1.getRoles().add(new UserRoles(u2, r3));
+
+
+        u2.setUserid(102);
+        userList.add(u2);
+
+        // user
+        User u3 = new User("testingbarn", "ILuvM4th!", "testingbarn@school.lambda");
+        u3.getRoles().add(new UserRoles(u3, r1));
+
+
+        u3.setUserid(103);
+        userList.add(u3);
+
+        User u4 = new User("testingcat", "password", "testingcat@school.lambda");
+        u4.getRoles().add(new UserRoles(u4, r2));
+
+        u4.setUserid(104);
+        userList.add(u4);
+
+        User u5 = new User("testingdog", "password", "testingdog@school.lambda");
+        u4.getRoles().add(new UserRoles(u5, r2));
+
+        u5.setUserid(105);
+        userList.add(u5);
     }
 
-    @org.junit.After
+    @After
     public void tearDown() throws Exception {
     }
 
-    @org.junit.Test
-    public void listAllUsers() {
+    @Test
+    public void listAllUsers() throws Exception{
+        String apiUrl = "/users/users";
+        Mockito.when(userService.findAll()).thenReturn(userList);
+        RequestBuilder rb = MockMvcRequestBuilders.get(apiUrl).accept(MediaType.APPLICATION_JSON);
+        MvcResult r = mockMvc.perform(rb).andReturn();
+        String tr = r.getResponse().getContentAsString();
+
+        ObjectMapper mapper = new ObjectMapper();
+        String er = mapper.writeValueAsString(userList);
+
+
+        assertEquals(er, tr);
     }
 
-    @org.junit.Test
-    public void getUserById() {
+    @Test
+    public void getUserById()throws Exception {
+        String apiUrl = "/users/user/12";
+
+        Mockito.when(userService.findUserById(12))
+                .thenReturn(userList.get(1));
+
+        RequestBuilder rb = MockMvcRequestBuilders.get(apiUrl)
+                .accept(MediaType.APPLICATION_JSON);
+        MvcResult r = mockMvc.perform(rb)
+                .andReturn(); // this could throw an exception
+        String tr = r.getResponse()
+                .getContentAsString();
+
+        ObjectMapper mapper = new ObjectMapper();
+        String er = mapper.writeValueAsString(userList.get(1));
+
+        System.out.println("Expect: " + er);
+        System.out.println("Actual: " + tr);
+
+        assertEquals("Rest API Returns List", er, tr);
     }
 
-    @org.junit.Test
+    @Test
     public void addUser() {
     }
 
-    @org.junit.Test
+    @Test
     public void deleteUserById() {
     }
 }
--- shoppingcart/src/test/java/com/lambdaschool/shoppingcart/services/UserServiceImplTest.java
+++ shoppingcart/src/test/java/com/lambdaschool/shoppingcart/services/UserServiceImplTest.java
@@ -1,46 +1,83 @@
 package com.lambdaschool.shoppingcart.services;
 
+import com.lambdaschool.shoppingcart.ShoppingcartApplication;
+import com.lambdaschool.shoppingcart.exceptions.ResourceNotFoundException;
+import com.lambdaschool.shoppingcart.models.Role;
+import com.lambdaschool.shoppingcart.models.User;
+import com.lambdaschool.shoppingcart.models.UserRoles;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static junit.framework.TestCase.assertEquals;
 import static org.junit.Assert.*;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ShoppingcartApplication.class)
 public class UserServiceImplTest {
 
-    @org.junit.Before
+    @Autowired
+    private UserService userService;
+
+    @Before
     public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        List<User> mylist = userService.findAll();
+        for(User u : mylist) {
+            System.out.println(u.getUserid() + " " + u.getUsername());
+        }
     }
 
-    @org.junit.After
+    @After
     public void tearDown() throws Exception {
     }
 
-    @org.junit.Test
+    @Test
     public void findUserById() {
+        assertEquals("barnbarn",
+                userService.findUserById(1).getUsername());
+    }
+    @Test(expected = ResourceNotFoundException.class)
+    public void findUserByIdNotFound() {
+        assertEquals("", userService.findUserById(200).getUsername());
     }
 
-    @org.junit.Test
+    @Test
     public void findByNameContaining() {
     }
 
-    @org.junit.Test
+    @Test
     public void findAll() {
+        assertEquals(3,userService.findAll().size());
     }
 
-    @org.junit.Test
+    @Test
     public void delete() {
     }
 
-    @org.junit.Test
+    @Test
     public void findByName() {
     }
 
-    @org.junit.Test
+    @Test
     public void save() {
     }
 
-    @org.junit.Test
+    @Test
     public void update() {
     }
 
-    @org.junit.Test
+    @Test
     public void deleteAll() {
     }
 }
